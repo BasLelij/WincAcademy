901ddf526593923c677277f56c0247605d59b513
Bas van der Lely <106604607+BasLelij@users.noreply.github.com>
2023-01-12 14:42:08 +0100

    Add files via upload

---
+++ todolist2/api-client.js
@@ -0,0 +1,31 @@
+// // get request
+
+// fetch("http://localhost:3000", {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
+//   .then((response) => response.text())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+// // post request
+
+//   const data = { description: "buy oatmeal", done: false };
+
+// fetch("http://localhost:3000/", {
+//   method: "POST",
+//   body: JSON.stringify(data),
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+//   // delete request
